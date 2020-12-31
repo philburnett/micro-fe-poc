3f7ca73a4219be1eb14274e777c5a840f57fe24b
Phil Burnett <phil_burnett@discovery.com>
2020-12-31 09:31:00 +0000

    Testing workflow

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,10 +3,9 @@ import MarketingApp from './components/MarketingApp';
 
 export default () => {
     return (<div>
-        <h1>Hello</h1>
+        <h1>Hello Phil</h1>
         <hr />
         <MarketingApp />
     </div>
-
     );
 };
